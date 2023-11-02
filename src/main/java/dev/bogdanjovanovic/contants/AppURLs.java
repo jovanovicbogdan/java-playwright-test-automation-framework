91c82370b8695bba95e8c879ecce025448453d30
Bogdan Jovanovic <jbogdan1998@hotmail.com>
2023-11-02 19:51:41 +0100

    feat(openai): add openai base api url constant and chat completions endpoint v1

--- src/main/java/dev/bogdanjovanovic/contants/AppURLs.java
+++ src/main/java/dev/bogdanjovanovic/contants/AppURLs.java
@@ -2,6 +2,8 @@ package dev.bogdanjovanovic.contants;
 
 public class AppURLs {
 
+  public static final String OPENAI_BASE_API_URL = "https://api.openai.com";
+
   public static final String SWAG_LABS_HOME_PAGE = "https://www.saucedemo.com/";
   public static final String TODO_MVC_HOME_PAGE = "https://demo.playwright.dev/todomvc/";
   public static final String WEB_FORM_PAGE = "https://www.selenium.dev/selenium/web/web-form.html";
--- src/main/java/dev/bogdanjovanovic/helpers/OpenAI.java
+++ src/main/java/dev/bogdanjovanovic/helpers/OpenAI.java
@@ -5,9 +5,9 @@ import dev.bogdanjovanovic.models.ChatCompletion;
 public class OpenAI {
 
   final String OPENAI_API_KEY = System.getenv("OPENAI_API_KEY");
+  final String CHAT_COMPLETIONS_ENDPOINT_V1 = "/v1/chat/completions";
 
   public void chatCompletion(final ChatCompletion prompt) {
-
   }
 
 }
